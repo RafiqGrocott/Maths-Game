f553607ff8152ec7e690a032ecda671132a73971
RafiqGrocott <99296457+RafiqGrocott@users.noreply.github.com>
2022-06-16 10:56:46 +1200

    Add files via upload

--- 03_Addition_Subtraction_Mode.py
+++ 03_Addition_Subtraction_Mode.py
@@ -1,15 +1,17 @@
-def choice_checker(error, question, valid_list, addition_list, subtraction_list):
+def choice_checker(error, add_sub_mode, add_sub_list):
+
+    add_sub_list = ["+", "-", "take away"]
 
     valid = False
     while not valid:
 
         # Ask user for choice (and put in lowercase)
-        response = input(question).lower()
+        response = input(add_sub_mode).lower()
 
         # Iterates through list and if response us an item
         # In the list (or the first letter of an item), the
         # Full item name is returned
-        for item in valid_list:
+        for item in add_sub_list:
             if response == item[0] or response == item:
                 return item
         
@@ -20,8 +22,14 @@ def choice_checker(error, question, valid_list, addition_list, subtraction_list)
 
         return response
 
-addition_list = ["+", "addition", "plus"]
-subtraction_list = ["-", "subtraction", "take away", "minus"]
+add_sub_list = ["+", "-", "take away"]
 
-add_sub_mode = choice_checker("Would you like to play the addition game or the subtraction game? ", addition_list, subtraction_list, "Please enter '+', '-', or 'take away'")
+# Ask user if they want to play addition or subtraction mode
+add_sub_mode = choice_checker("Would you like to play the addition game or the subtraction game? Please enter + / - ", add_sub_list, "Please enter +, -, or take away")
 print()
+
+if add_sub_mode == "+":
+    print("You chose addition mode!")
+
+elif add_sub_mode:
+    print("You chose subtraction mode!")
