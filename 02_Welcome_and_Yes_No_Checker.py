b0c2f52b52f2ba7069a45d6508853b91390d15cc
RafiqGrocott <99296457+RafiqGrocott@users.noreply.github.com>
2022-06-15 12:19:48 +1200

    Add files via upload

--- 02_Welcome_and_Yes_No_Checker.py
+++ 02_Welcome_and_Yes_No_Checker.py
@@ -3,7 +3,20 @@ print()
 
 # checks users choice based on a list
 
-yes_no_list = ["yes", "no"]
+def instructions():
+    print("*****Instrustions*****")
+    print()
+    print("Rules of the game...")
+    print()
+    print("You will be asked addition and subtraction questions.")
+    print()
+    print("As you go along, the code will calculate the number of rounds you've won and lost"
+        "and will eventually tell you how many you won and lost, and also the percentage of rounds you won and lost.")
+    print()
+    print("Won = Answered question correctly. ")
+    print()
+    print("Lost = Answered question incorrectly.")
+
 
 def choice_checker (question, valid_list, error):
 
@@ -25,8 +38,7 @@ def choice_checker (question, valid_list, error):
         print(error)
         print()
 
-
-instructions = "Play the game"
+yes_no_list = ["yes", "no"]
 
 played_before = choice_checker("Have you played this game before? Please enter yes or no. ", yes_no_list, "Please enter yes / no")
 print()
--- 03_Addition_Subtraction_Mode.py
+++ 03_Addition_Subtraction_Mode.py
@@ -1,1 +1,27 @@
-03_Addition_Subtraction_Mode.py
+def choice_checker(error, question, valid_list, addition_list, subtraction_list):
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice (and put in lowercase)
+        response = input(question).lower()
+
+        # Iterates through list and if response us an item
+        # In the list (or the first letter of an item), the
+        # Full item name is returned
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+        
+        # Output error if item not in list
+        print("item is not in list")
+        print(error)
+        print()
+
+        return response
+
+addition_list = ["+", "addition", "plus"]
+subtraction_list = ["-", "subtraction", "take away", "minus"]
+
+add_sub_mode = choice_checker("Would you like to play the addition game or the subtraction game? ", addition_list, subtraction_list, "Please enter '+', '-', or 'take away'")
+print()
